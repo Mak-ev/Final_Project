a174ae659868403377524a7a27a11f08fabbf651
Makayla E <Strawberryanimation10186@gmail.com>
2023-06-28 13:01:32 -0400

    Initial Commit

---
+++ Final_Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final_Project
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
